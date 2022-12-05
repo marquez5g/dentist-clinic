e26f22fec0ca75c4e0579e5da27566f0f4d7f87a
Gabriel Marquez <marquez122@outlook.com>
2022-12-05 18:26:37 -0500

    patient repository

---
+++ src/main/java/com/dh/dentistclinic/repositories/PatientRepository.java
@@ -0,0 +1,7 @@
+package com.dh.dentistclinic.repositories;
+
+import com.dh.dentistclinic.entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
