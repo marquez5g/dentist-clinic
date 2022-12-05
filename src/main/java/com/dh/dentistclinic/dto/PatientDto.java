7895a626e2ac8dc230ac1d7c59865a43c102954a
Gabriel Marquez <marquez122@outlook.com>
2022-12-05 18:26:50 -0500

    patient dto

--- src/main/java/com/dh/dentistclinic/dto/PatientDto.java
+++ src/main/java/com/dh/dentistclinic/dto/PatientDto.java
@@ -1,5 +1,6 @@
 package com.dh.dentistclinic.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 @Setter
 @Getter
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PatientDto {
     private String name;
     private String lastName;
