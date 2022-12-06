392e961b1eb8c91fcb849698b276374614a6ac69
Gabriel Marquez <marquez122@outlook.com>
2022-12-06 18:39:06 -0500

    patient service interface

---
+++ src/main/java/com/dh/dentistclinic/services/PatientService.java
@@ -0,0 +1,15 @@
+package com.dh.dentistclinic.services;
+
+import com.dh.dentistclinic.dto.PatientDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PatientService {
+    PatientDto save(PatientDto user);
+    void delete(PatientDto user);
+    PatientDto getByDni(String username);
+    List<PatientDto> getAll();
+    PatientDto update(PatientDto user);
+}
