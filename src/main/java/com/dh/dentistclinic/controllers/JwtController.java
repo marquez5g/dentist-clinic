417df7305d22297475492504e15e845377ee0190
Gabriel Marquez <marquez122@outlook.com>
2022-12-07 19:00:07 -0500

    Controller to handle jwt

---
+++ src/main/java/com/dh/dentistclinic/controllers/JwtController.java
@@ -0,0 +1,45 @@
+package com.dh.dentistclinic.controllers;
+
+import com.dh.dentistclinic.entities.jwt.AuthenticationRequest;
+import com.dh.dentistclinic.entities.jwt.AuthenticationResponse;
+import com.dh.dentistclinic.security.AppUserService;
+import com.dh.dentistclinic.security.config.jwt.JwtUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JwtController {
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+
+    public JwtController(AuthenticationManager authenticationManager, AppUserService userDetailsService, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+            throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+                            authenticationRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect", e);
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse((jwt)));
+    }
+}
