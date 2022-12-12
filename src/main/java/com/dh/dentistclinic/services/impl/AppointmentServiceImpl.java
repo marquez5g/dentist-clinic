8fc39335a568c4f67b333a8e4ebf412362f3eb8f
Gabriel Marquez <marquez122@outlook.com>
2022-12-12 08:49:02 -0500

    appointment service implementation

--- src/main/java/com/dh/dentistclinic/services/impl/AppointmentServiceImpl.java
+++ src/main/java/com/dh/dentistclinic/services/impl/AppointmentServiceImpl.java
@@ -2,30 +2,72 @@ package com.dh.dentistclinic.services.impl;
 
 import com.dh.dentistclinic.dto.AppointmentDto;
 import com.dh.dentistclinic.entities.Appointment;
+import com.dh.dentistclinic.entities.Dentist;
+import com.dh.dentistclinic.entities.Patient;
+import com.dh.dentistclinic.repositories.AppointmentRepository;
+import com.dh.dentistclinic.repositories.DentistRepository;
+import com.dh.dentistclinic.repositories.PatientRepository;
 import com.dh.dentistclinic.services.AppointmentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AppointmentServiceImpl implements AppointmentService {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private final AppointmentRepository appointmentRepository;
+
+    private final PatientRepository patientRepository;
+
+    private final DentistRepository dentistRepository;
+
+
+
+    public AppointmentServiceImpl(AppointmentRepository appointmentRepository, PatientRepository patientRepository, DentistRepository dentistRepository) {
+        this.appointmentRepository = appointmentRepository;
+        this.patientRepository = patientRepository;
+        this.dentistRepository = dentistRepository;
+    }
+
     @Override
-    public AppointmentDto save(Appointment appointment) {
-        return null;
+    public AppointmentDto save(AppointmentDto appointmentDto) {
+        Patient patient = patientRepository.findByDni(appointmentDto.getPatient().getDni());
+        Dentist dentist = dentistRepository.findByCredential(appointmentDto.getDentist().getCredential());
+        Appointment appointment = mapToEntity(appointmentDto);
+        appointment.setPatient(patient);
+        appointment.setDentist(dentist);
+        return mapToDto(appointmentRepository.save(appointment));
     }
 
     @Override
     public void delete(AppointmentDto appointment) {
-
+        appointmentRepository.delete(mapToEntity(appointment));
     }
 
     @Override
     public List<AppointmentDto> getAll() {
-        return null;
+        return appointmentRepository.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
     }
 
     @Override
-    public AppointmentDto update(AppointmentDto appointment) {
-        return null;
+    public List<AppointmentDto> getAllByDentistCredential(String credential) {
+        return appointmentRepository.findAllByDentistCredential(credential).stream().map(this::mapToDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<AppointmentDto> getAllByPatientDni(String dni) {
+        return appointmentRepository.findAllByPatientDni(dni).stream().map(this::mapToDto).collect(Collectors.toList());
+    }
+
+    private AppointmentDto mapToDto (Appointment appointment) {
+        return mapper.convertValue(appointment, AppointmentDto.class);
+    }
+
+    private Appointment mapToEntity (AppointmentDto appointmentDto) {
+        return mapper.convertValue(appointmentDto, Appointment.class);
     }
 }
