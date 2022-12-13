a160a6967ce3347a7eb1f6ccad905a7c93a2b2de
Gabriel Marquez <marquez122@outlook.com>
2022-12-12 19:01:36 -0500

    adds exceptions handling

--- src/main/java/com/dh/dentistclinic/services/AppointmentService.java
+++ src/main/java/com/dh/dentistclinic/services/AppointmentService.java
@@ -1,12 +1,14 @@
 package com.dh.dentistclinic.services;
 
 import com.dh.dentistclinic.dto.AppointmentDto;
+import com.dh.dentistclinic.exceptions.EntityNotFoundException;
+
 import java.util.List;
 
 public interface AppointmentService {
     AppointmentDto save(AppointmentDto user);
     void delete(AppointmentDto user);
     List<AppointmentDto> getAll();
-    List<AppointmentDto> getAllByDentistCredential(String credential);
-    List<AppointmentDto> getAllByPatientDni(String dni);
+    List<AppointmentDto> getAllByDentistCredential(String credential) throws EntityNotFoundException;
+    List<AppointmentDto> getAllByPatientDni(String dni) throws EntityNotFoundException;
 }
